34412733adf168b9aa1690b2a252069781cd1336
jyh <823213790@qq.com>
2024-09-06 22:39:44 +0800

    2024-09-06

---
+++ LeetCode/src/main/java/com/jyh/DynamicProgramming/LeetCode115.java
@@ -0,0 +1,23 @@
+package com.jyh.DynamicProgramming;
+
+
+public class LeetCode115 {
+    /**
+     * 115. 不同的子序列
+     * https://leetcode.cn/problems/distinct-subsequences/description/
+     *
+     */
+    public static void main(String[] args) {
+        Solution s1 = new Solution();
+        System.out.println(s1);
+
+    }
+
+    static class Solution {
+        public int numDistinct(String s, String t) {
+            long mod = (long) Math.pow(10,9) + 7;
+
+            return 0;
+        }
+    }
+}
